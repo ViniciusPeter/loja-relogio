bfb792cd7cb79f62dc03b64a30f260b030e0f5db
Vinícius Peter <vinicius.peter@outlook.com>
2024-02-13 13:54:27 -0300

    <Feat>: Colocando validação de letras, numeros e mínimo de 8 digitos obrigatórios

--- Loja/src/validations/stepPasswordRegister.ts
+++ Loja/src/validations/stepPasswordRegister.ts
@@ -1,21 +1,53 @@
 import { z } from "zod";
 
-const passwordContainsLetterOrNumber = (password: string) => {
-  const letterOrNumberRegex = /[a-zA-Z0-9]/;
-  return letterOrNumberRegex.test(password);
-};
+function passwordContainsLetter(password: string) {
+  let letter = false;
+
+  password.split("").forEach((caracter) => {
+    const validCaracter = Number(caracter);
+
+    if (isNaN(validCaracter)) {
+      return (letter = true);
+    }
+  });
+
+  return letter;
+}
+
+function passwordContainsNumber(password: string) {
+  let number = false;
+
+  password.split("").forEach((caracter) => {
+    const validCaracter = Number(caracter);
+
+    if (!isNaN(validCaracter)) {
+      return (number = true);
+    }
+  });
+
+  return number;
+}
 
 export const createStepPasswordResgiterSchema = z
   .object({
     password: z
       .string()
       .min(8, "A senha precisa ter no mínimo 8 dígitos")
-      .refine(passwordContainsLetterOrNumber, {
-        message: "A senha precisa conter ao menos letras e números",
+      .refine(passwordContainsLetter, {
+        message: "A senha precisa conter ao menos uma letra",
+      })
+      .refine(passwordContainsNumber, {
+        message: "A senha precisa conter ao menos um número",
       }),
     confirmPassword: z
       .string()
-      .min(8, "A senha precisa ter no mínimo 8 dígitos"),
+      .min(8, "A senha precisa ter no mínimo 8 dígitos")
+      .refine(passwordContainsLetter, {
+        message: "A senha precisa conter ao menos uma letra",
+      })
+      .refine(passwordContainsNumber, {
+        message: "A senha precisa conter ao menos um número",
+      }),
   })
   .refine((data) => data.password === data.confirmPassword, {
     message: "A senhas precisam ser iguais",
